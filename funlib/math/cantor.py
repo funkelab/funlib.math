78c587a6f20ba0041b2e3b4901b1ab22c6d8feab
Jan Funke <funkej@janelia.hhmi.org>
2019-01-09 18:59:50 -0500

    Add docstring to cantor_number

--- funlib/math/cantor.py
+++ funlib/math/cantor.py
@@ -1,6 +1,24 @@
 from __future__ import division
 
 
+def cantor_number(coordinate):
+    '''Return the "Cantor number" of tuples of arbitrary length.
+
+    The Cantor number is the generalization of Cantor's mapping of rational
+    numbers (2-tuples) to natural numbers for arbitrary dimensions.
+
+    The numbers obtained this way can be used to uniquely index a positive
+    integer coordinate in a volume of unknown size.
+    '''
+
+    if len(coordinate) == 1:
+        return coordinate[0] + 1
+
+    return (
+        pyramide_volume(len(coordinate), sum(coordinate) - 1) +
+        cantor_number(coordinate[:-1]))
+
+
 def pyramide_volume(dims, edge_length):
 
     prod = 1
@@ -10,13 +28,3 @@ def pyramide_volume(dims, edge_length):
         norm *= d + 1
 
     return prod/norm
-
-
-def cantor_number(coordinate):
-
-    if len(coordinate) == 1:
-        return coordinate[0] + 1
-
-    return (
-        pyramide_volume(len(coordinate), sum(coordinate) - 1) +
-        cantor_number(coordinate[:-1]))
